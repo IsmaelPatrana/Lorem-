5ad8b53ce648cbd09035fdbf9df1100655591cc0
IsmaelBack <72406061+IsmaelPatrana@users.noreply.github.com>
2020-11-06 15:59:23 -0500

    Add files via upload

---
+++ trabavc/public/js/alerts.js
@@ -0,0 +1,14 @@
+$("#ingresar").click(function(){
+    Swal.fire({
+        title: 'Esta funcion no esta disponible por el momento.',
+        width: 600,
+        padding: '5em',
+        background: '#fff url(/img/imagen_600x500.png)', //es el fondo de la caja de dialogo
+        backdrop: `
+        rgba(5, 5, 25, 0.4)
+        url("https://sweetalert2.github.io/images/nyan-cat.gif")
+        center left
+        no-repeat
+        `
+    });
+});
